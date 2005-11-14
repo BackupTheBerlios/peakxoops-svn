dae417573701e3d3a3f7324f23ebb947a8391e9f
gijoe <gijoe@e2f808cc-50f8-0310-90e1-e20752c72bdc>
2005-11-14 18:44:36 +0000

    sitemap-1.23pre

--- trunk/html/modules/sitemap/plugins/myAds.php
+++ trunk/html/modules/sitemap/plugins/myAds.php
@@ -6,7 +6,7 @@
 
 function b_sitemap_myAds(){
 	$xoopsDB =& Database::getInstance();
-	$block = sitemap_get_categoires_map($xoopsDB->prefix("ann_categories"), "cid", "pid", "title", "index.php?pa=view&cid=", "title");
+	$block = sitemap_get_categoires_map($xoopsDB->prefix("ann_categories"), "cid", "pid", "title", "index.php?pa=view&amp;cid=", "title");
 	return $block;
 }
 

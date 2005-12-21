cb3bd609510ea15efa45c8b500948d0b3adda18e
gijoe <gijoe@e2f808cc-50f8-0310-90e1-e20752c72bdc>
2005-12-21 10:07:51 +0000

    tplsadmin-0.1

---
+++ trunk/html/modules/tplsadmin/language/english/modinfo.php
@@ -0,0 +1,6 @@
+<?php
+
+define( '_MI_TPLSADMIN_MODULENAME' , 'Templates admin' ) ;
+define( '_MI_TPLSADMIN_MODULEDESC' , 'Customize your template set easy as possible!' ) ;
+
+?>
---
+++ trunk/html/modules/tplsadmin/language/japanese/modinfo.php
@@ -0,0 +1,6 @@
+<?php
+
+define( '_MI_TPLSADMIN_MODULENAME' , 'テンプレート管理' ) ;
+define( '_MI_TPLSADMIN_MODULEDESC' , 'もっと使いやすいテンプレート管理を！' ) ;
+
+?>

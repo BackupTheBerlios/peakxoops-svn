6663ff0c9a04e62a230fd632c180bd8f53a9af45
gijoe <gijoe@e2f808cc-50f8-0310-90e1-e20752c72bdc>
2006-04-05 20:05:50 +0000

    Protector-2.56a

--- trunk/html/modules/protector/language/german/admin.php
+++ trunk/html/modules/protector/language/german/admin.php
@@ -15,34 +15,34 @@ define("_AM_TH_DESCRIPTION","Beschreibung");
 define( "_AM_TH_BADIPS" , "\"Schlechte\" IPs" ) ;
 define( "_AM_TH_ENABLEIPBANS" , "IP-Sperre aktivieren?" ) ;
 
-define( "_AM_LABEL_REMOVE" , "Markierte EintrçÈe l‹Ôchen:" ) ;
+define( "_AM_LABEL_REMOVE" , "Markierte Einträge l‹Ôchen:" ) ;
 define( "_AM_BUTTON_REMOVE" , "Entfernen!" ) ;
 define( "_AM_JS_REMOVECONFIRM" , "Entfernen OK?" ) ;
 define( "_AM_MSG_PRUPDATED" , "Einstellungen erfolgreich aktualisiert!" ) ;
-define( "_AM_MSG_REMOVED" , "EintrçÈe wurden entfernnt." ) ;
+define( "_AM_MSG_REMOVED" , "Einträge wurden entfernt." ) ;
 
 
 // prefix_manager.php
 define( "_AM_H3_PREFIXMAN" , "Prefix Manager" ) ;
 define( "_AM_MSG_DBUPDATED" , "Datenbank wurde erfolgreich aktualisiert!" ) ;
-define( "_AM_CONFIRM_DELETE" , "Alle Daten werden gel‹Ôcht. OK?" ) ;
-define( "_AM_TXT_HOWTOCHANGEDB" , "Wenn Sie den PrçÇix çÏdern wollen,<br /> bearbeiten Sie %s/mainfile.php manuell.<br /><br />define('XOOPS_DB_PREFIX', '<b>%s</b>');" ) ;
+define( "_AM_CONFIRM_DELETE" , "Alle Daten werden gelöscht. OK?" ) ;
+define( "_AM_TXT_HOWTOCHANGEDB" , "Wenn Sie den Präfix ändern wollen,<br /> bearbeiten Sie %s/mainfile.php manuell.<br /><br />define('XOOPS_DB_PREFIX', '<b>%s</b>');" ) ;
 
 
 // advisory.php
 define("_AM_ADV_NOTSECURE","Nicht sicher");
 
-define("_AM_ADV_REGISTERGLOBALS","Diese Einstellung lçÅt zu verschiedenen Formen der Code Injection ein.<br />Wenn es geht, setzen Sie eine .htaccess-Datei.");
-define("_AM_ADV_ALLOWURLFOPEN","Diese Einstellung erlaubt Angreifern, willk—Ólich Scripts auf entfernten Sytemen auszuf—Éren.<br />Nur der Administrator des Servers kann diese Option çÏdern.<br />Wenn Sie Admin sind, bearbeiten Sie php.ini or httpd.conf entsprechend.<br /><b>Beispiel f—Ó httpd.conf:<br /> &nbsp; php_admin_flag &nbsp; allow_url_fopen &nbsp; off</b><br />Wenn nicht, wenden Sie sich an Ihren Administrator.");
-define("_AM_ADV_DBPREFIX","Diese Einstellung lçÅt zu 'SQL Injections' ein.<br />Vergessen Sie nicht 'Force sanitizing *' in den Voreinstellungen dieses Moduls zu aktivieren.");
-define("_AM_ADV_LINK_TO_PREFIXMAN","Zum PrçÇix-Manager");
-define("_AM_ADV_MAINUNPATCHED","Xoops Protector kann ihre Seite unter bestimmten UmstçÏden sch—Õzen, wenn es aus der mainfile.php aufgerufen wird.<br />Sie sollten diese Datei wie im README beschrieben çÏdern.");
+define("_AM_ADV_REGISTERGLOBALS","Diese Einstellung lädt zu verschiedenen Formen der Code Injection ein.<br />Wenn es geht, setzen Sie eine .htaccess-Datei.");
+define("_AM_ADV_ALLOWURLFOPEN","Diese Einstellung erlaubt Angreifern, willkuerlich Scripts auf entfernten Sytemen auszufuehren.<br />Nur der Administrator des Servers kann diese Option ändern.<br />Wenn Sie der Admin sind, bearbeiten Sie php.ini or httpd.conf entsprechend.<br /><b>Beispiel für httpd.conf:<br /> &nbsp; php_admin_flag &nbsp; allow_url_fopen &nbsp; off</b><br />Wenn nicht, wenden Sie sich an Ihren Administrator.");
+define("_AM_ADV_DBPREFIX","Diese Einstellung lädt zu 'SQL Injections' ein.<br />Vergessen Sie nicht 'Force sanitizing *' in den Voreinstellungen dieses Moduls zu aktivieren.");
+define("_AM_ADV_LINK_TO_PREFIXMAN","Zum Präfix-Manager");
+define("_AM_ADV_MAINUNPATCHED","Xoops Protector kann ihre Seite unter bestimmten Umständen schützen, wenn es aus der mainfile.php aufgerufen wird.<br />Sie sollten diese Datei wie im README beschrieben ändern.");
 define("_AM_ADV_RESCUEPASSWORD","Notfall-Passwort");
-define("_AM_ADV_RESCUEPASSWORDUNSET","L‹Ôchen");
+define("_AM_ADV_RESCUEPASSWORDUNSET","Löschen");
 define("_AM_ADV_RESCUEPASSWORDSHORT","Passwort zu kurz (min 6 Zeichen)");
 
-define("_AM_ADV_SUBTITLECHECK","ŽÜberpr—Çen, ob Protector funktioniert");
-define("_AM_ADV_AT1STSETPASSWORD","Zuerst m—Ôsen Sie ein Notfall-Passwort angeben.");
+define("_AM_ADV_SUBTITLECHECK","Überprüfen, ob Protector funktioniert");
+define("_AM_ADV_AT1STSETPASSWORD","Zuerst müssen Sie ein Notfall-Passwort angeben.");
 define("_AM_ADV_CHECKCONTAMI","Verseuchung");
 define("_AM_ADV_CHECKISOCOM","Isolierte Kommentare");
 
--- trunk/html/modules/protector/language/german/modinfo.php
+++ trunk/html/modules/protector/language/german/modinfo.php
@@ -1,105 +1,110 @@
 <?php
 // Module Info
 
-// The name of this module
+// The name ofürthis module
 
 
 
 
 
+
+
+// Appended by Xoops Language Checker -GIJOE- in 2006-04-06 05:04:58
+define('_MI_PROTECTOR_BF_COUNT','Anti Brute Force');
+define('_MI_PROTECTOR_BF_COUNTDSC','Set count you allow guest try to login within 10 minutes. If someone fails to login more than this number, her/his IP will be banned.');
 
 // Appended by Xoops Language Checker -GIJOE- in 2005-08-24 13:15:38
-define('_MI_PROTECTOR_HIJACK_TOPBIT','Protected IP bits for the session');
-define('_MI_PROTECTOR_HIJACK_TOPBITDSC','Anti Session Hi-Jacking:<br />Default 32(bit). (All bits are protected)<br />When your IP is not stable, set the IP range by number of the bits.<br />(eg) If your IP can move in the range of 192.168.0.0-192.168.0.255, set 24(bit) here');
+define('_MI_PROTECTOR_HIJACK_TOPBIT','Geschützte IP bits für dieses Session');
+define('_MI_PROTECTOR_HIJACK_TOPBITDSC','Anti Session Hi-Jacking:<br />Default 32(bit). (Alle Bits sind geschützt)<br />Wenn Sie keine statische IP Adresse haben, setzen Sie den IP Bereich mit Nummer der einzelnen Bits.<br />(eg) Wenn sich Ihre IP im Bereich von 192.168.0.0 bis 192.168.0.255 befindet, setzen Sie 24(bit) hier');
 
 // Appended by Xoops Language Checker -GIJOE- in 2005-07-31 12:33:21
-define('_MI_PROTECTOR_DISABLES','Disable dangerous features in XOOPS');
+define('_MI_PROTECTOR_DISABLES','Deaktiviert die Sicherheitsfeatures von Protector in XOOPS');
 
 // Appended by Xoops Language Checker -GIJOE- in 2005-07-22 15:35:35
 define('_MI_PROTECTOR_RELIABLE_IPS','Reliable IPs');
-define('_MI_PROTECTOR_RELIABLE_IPSDSC','set IPs you can rely separated with | . ^ matches the head of string, $ matches the tail of string.');
-define('_MI_PROTECTOR_BF_COUNT','Anti Brute Force');
-define('_MI_PROTECTOR_BF_COUNTDSC','Set count you allow guest try to login within 10 minutes. If someone fails to login more than this number, her/his IP will be banned.');
-define('_MI_PROTECTOR_DOS_SKIPMODS','Modules out of DoS/Crawler checker');
-define('_MI_PROTECTOR_DOS_SKIPMODSDSC','set the dirnames of the modules separated with |. This option will be useful with chatting module etc.');
+define('_MI_PROTECTOR_RELIABLE_IPSDSC','Sie können IP Adressen mit einem | trennen. ^ setzt den Kopfürdes String, $ setzt das Ende des Strings.');
+define('_MI_PROTECTOR_Bfür_COUNT','Anti Brute Force');
+define('_MI_PROTECTOR_Bfür_COUNTDSC','Setzt die Anzahl, wie oft sich Gäste innerhalb 10 min anmelden dürfen. Wenn sich jemand öfter als hier angegeben anmeldet, wird dessen IP gesperrt.');
+define('_MI_PROTECTOR_DOS_SKIPMODS','Modul out ofürDoS/Crawler Checker');
+define('_MI_PROTECTOR_DOS_SKIPMODSDSC','setzt die Verzeichnisnamen der Module, getrennt durch ein |. Diese Option ist bei Chatmodulen etc. hilfreich');
 
 // Appended by Xoops Language Checker -GIJOE- in 2005-03-31 12:07:31
-define('_MI_PROTECTOR_PREFIXMANAGER','Prefix Manager');
+define('_MI_PROTECTOR_PREFIXMANAGER','Präfix Manager');
 
 // Appended by Xoops Language Checker -GIJOE- in 2005-03-05 07:09:09
-define('_MI_PROTECTOR_GLOBAL_DISBL','Temporary disabled');
-define('_MI_PROTECTOR_GLOBAL_DISBLDSC','All protections are disabled in temporary.<br />Don\'t forget turn this off after shooting the trouble');
+define('_MI_PROTECTOR_GLOBAL_DISBL','Vorübergehend deaktiviert');
+define('_MI_PROTECTOR_GLOBAL_DISBLDSC','Alle Sicherheitsfunktionen sind vorübergehend deaktiviert!<br />Vergessen Sie nicht diese wieder einzuschalten, wenn Sie eine Störung beseitigt haben!');
 define('_MI_PROTECTOR_LOG_LEVEL','Logging level');
 define('_MI_PROTECTOR_LOG_LEVELDSC','');
-define('_MI_PROTECTOR_LOGLEVEL0','none');
-define('_MI_PROTECTOR_LOGLEVEL15','Quiet');
-define('_MI_PROTECTOR_LOGLEVEL63','quiet');
-define('_MI_PROTECTOR_LOGLEVEL255','full');
+define('_MI_PROTECTOR_LOGLEVEL0','nichts');
+define('_MI_PROTECTOR_LOGLEVEL15','still');
+define('_MI_PROTECTOR_LOGLEVEL63','still');
+define('_MI_PROTECTOR_LOGLEVEL255','voll');
 
 // Appended by Xoops Language Checker -GIJOE- in 2005-02-18 18:41:12
-define('_MI_PROTECTOR_DOSOPT_HTA','DENY by .htaccess(Experimental)');
+define('_MI_PROTECTOR_DOSOPT_HTA','DENY by .htaccess(experimental)');
 
 define("_MI_PROTECTOR_NAME","Xoops Protector");
 
-// A brief description of this module
-define("_MI_PROTECTOR_DESC","Dieses Modul sch—Õzt vor Angriffen auf Ihre Xoops-Seite (DoS , SQL Injection , und Variables contaminations)<br />Sie m—Ôsen diesen Block oben links anzeigen lassen und den anonymen Usern den Zugriff auf diesen Block gestatten.");
+// A briefürdescription ofürthis module
+define("_MI_PROTECTOR_DESC","Dieses Modul schützt vor Angriffen aufürIhre Xoops-Seite (DoS , SQL Injektion und Variablen Infektionen)<br />Sie müssen diesen Block oben links anzeigen lassen und den anonymen Usern den Zugriffüraufürdiesen Block gestatten.");
 
-// Names of blocks for this module (Not all module has blocks)
+// Names ofürblocks for this module (Not all module has blocks)
 define("_MI_PROTECTOR_BNAME1","Protector");
-define("_MI_PROTECTOR_BDESC1","Dieser Block sollte oben links stehen und auf ALLEN SEITEN angezeigt werden.");
+define("_MI_PROTECTOR_BDESC1","Dieser Block sollte oben links stehen und aufürALLEN SEITEN angezeigt werden.");
 
 // Menu
 define("_MI_PROTECTOR_ADMININDEX","Protect Center");
 define("_MI_PROTECTOR_ADVISORY","Sicherheitsberatung");
-define("_MI_PROTECTOR_MYBLOCKSADMIN","Block&Gruppen Admin");
+define("_MI_PROTECTOR_MYBLOCKSADMIN","Block & Gruppen Admin");
 
 // Configs
-define('_MI_PROTECTOR_HIJACK_DENYGP','Gruppen denen das çÏdern der IP innerhalb einer Session untersagt wird.');
-define('_MI_PROTECTOR_HIJACK_DENYGPDSC','Anti Session Hi-Jacking:<br />WçÉlen sie Gruppen aus, denen es untersagt ist, ihre IP wçÉrend einer Session zu çÏdern..<br />(Mindestens Administrator-Gruppe wird empfohlen.)');
-define('_MI_PROTECTOR_SAN_NULLBYTE','Sanitizing (SçÖberung) null-bytes');
+define('_MI_PROTECTOR_HIJACK_DENYGP','Gruppen denen das Ändern der IP innerhalb einer Session untersagt wird.');
+define('_MI_PROTECTOR_HIJACK_DENYGPDSC','Anti Session Hi-Jacking:<br />Wählen sie Gruppen aus, denen es untersagt ist, ihre IP während einer Session zu ändern..<br />(Mindestens Administrator-Gruppe wird empfohlen.)');
+define('_MI_PROTECTOR_SAN_NULLBYTE','Sanitizing (Säuberung) null-bytes');
 define('_MI_PROTECTOR_SAN_NULLBYTEDSC','Das Abschluss-Zeichen "\\0" wird oft in Attacken verwendet.<br />Dieses Null-Byte wird in ein Leerzeichen konvertiert.<br />(Einschalten wird dringendst empfohlen!)');
 define('_MI_PROTECTOR_DIE_NULLBYTE','Beenden, wenn Null-Bytes gefunden werden');
-define('_MI_PROTECTOR_DIE_NULLBYTEDSC','TDas Abschluss-Zeichen "\\0" wird oft in Attacken verwendet.<br />(Dringendst empfohlen)');
-define('_MI_PROTECTOR_DIE_BADEXT','Beenden, wenn unzulçÔsige Dateien hochgeladen werden');
-define('_MI_PROTECTOR_DIE_BADEXTDSC','Wenn jemand versucht, Dateien mit unzulçÔsigen Endungen wie .php hochzuladen, beendet diese Modul den Zugriff auf XOOPS.<br />Wenn Sie oft Dateien in B-Wiki oder PukiWikiMod einstellen, schalten Sie diese Option aus.');
-define('_MI_PROTECTOR_CONTAMI_ACTION','Massnahmen, wenn eine Verunreinigung gefunden wurde:');
-define('_MI_PROTECTOR_CONTAMI_ACTIONDS','WçÉlen Sie eine Aktion aus, wenn jemand versucht, globale XOOPS-Variablen zu verunreinigen.<br />(Empfohlen wird "Weisser Bildschirm")');
-define('_MI_PROTECTOR_ISOCOM_ACTION','AMassnahmen, wenn eine isolierte Einkommentierung gefunden wurde:');
-define('_MI_PROTECTOR_ISOCOM_ACTIONDSC','Anti SQL Injection:<br />WçÉlen Sie eine Massnahme aus, die ergriffen wird, wenn ein  isoliertes "/*" gefunden wird.<br />"Sanitizing (SçÖberung)" bedeutet, ein zusçÕzliches  "*/" anzuhçÏgen.<br />(Empfohlen wird "Sanitizing (SçÖberung)" )');
+define('_MI_PROTECTOR_DIE_NULLBYTEDSC','Das Abschluss-Zeichen "\\0" wird oft in Attacken verwendet.<br />(Dringendst empfohlen)');
+define('_MI_PROTECTOR_DIE_BADEXT','Beenden, wenn unzulässgige Dateien hochgeladen werden');
+define('_MI_PROTECTOR_DIE_BADEXTDSC','Wenn jemand versucht, Dateien mit unzulässigen Endungen wie .php hochzuladen, beendet diese Modul den ZugriffüraufürXOOPS.<br />Wenn Sie oft Dateien in B-Wiki oder PukiWikiMod einstellen, schalten Sie diese Option aus.');
+define('_MI_PROTECTOR_CONTAMI_ACTION','Maßnahmen, wenn eine Verunreinigung gefunden wurde:');
+define('_MI_PROTECTOR_CONTAMI_ACTIONDS','Wählen Sie eine Aktion aus, wenn jemand versucht, globale XOOPS-Variablen zu verunreinigen.<br />(Empfohlen wird "Weißer Bildschirm")');
+define('_MI_PROTECTOR_ISOCOM_ACTION','Maßnahmen, wenn eine isolierte Einkommentierung gefunden wurde:');
+define('_MI_PROTECTOR_ISOCOM_ACTIONDSC','Anti SQL Injection:<br />Wählen Sie eine Massnahme aus, die ergriffen wird, wenn ein  isoliertes "/*" gefunden wird.<br />"Sanitizing (Säuberung)" bedeutet, ein zusätzliches  "*/" anzuhängen.<br />(Empfohlen wird "Sanitizing (Säuberung)" )');
 define('_MI_PROTECTOR_UNION_ACTION','Massnahme wenn ein UNION gefunden wurde.');
-define('_MI_PROTECTOR_UNION_ACTIONDSC','Anti SQL Injection:<br />WçÉlen sie eine Massnahme, wenn ein SQL-Befehl wie UNION gefunden wurde.<br />"Sanitizing (SçÖberung)" bedeutet die ŽÄnderung von "union" nach "uni-on".<br />(Empfohlen wird Sanitizing (SçÖberung))');
-define('_MI_PROTECTOR_ID_INTVAL','Erzwinge intval f—Ó Variablen wie IDŽ´s');
-define('_MI_PROTECTOR_ID_INTVALDSC','Alle Anfragen mit Namen "*id" Werden als Integer behandelt.<br />Diese Option besch—Õzt sie vor einigen Arten der XSS-(Cross Site Scripting-) und SQL-Injection-Attacken.<br />Obwohl empfohlen wird, diese Option einzuschalten, kann es in einigen Modulen Probleme damit geben.');
+define('_MI_PROTECTOR_UNION_ACTIONDSC','Anti SQL Injection:<br />Wählen sie eine Massnahme, wenn ein SQL-Befehl wie UNION gefunden wurde.<br />"Sanitizing (Säuberung)" bedeutet die Änderung von "union" nach "uni-on".<br />(Empfohlen wird Sanitizing (Säuberung))');
+define('_MI_PROTECTOR_ID_INTVAL','Erzwinge intval für Variablen wie ID´s');
+define('_MI_PROTECTOR_ID_INTVALDSC','Alle Anfragen mit Namen "*id" Werden als Integer behandelt.<br />Diese Option beschützt sie vor einigen Arten der XSS-(Cross Site Scripting-) und SQL-Injection-Attacken.<br />Obwohl empfohlen wird, diese Option einzuschalten, kann es in einigen Modulen Probleme damit geben.');
 define('_MI_PROTECTOR_FILE_DOTDOT','Behebe zweifelhafte Dateiangaben');
 define('_MI_PROTECTOR_FILE_DOTDOTDSC','Eliminiertalle ".." aus Anfragen, die nach Dateien suchen');
 
-define('_MI_PROTECTOR_DOS_EXPIRE','Zeitlimit f—Ó hohe Serverlast (sec)');
-define('_MI_PROTECTOR_DOS_EXPIREDSC','Dieser Wert gibt das Zeitlimit f—Ó rasch wiederholte reloads der Seite (F5 Attacke) und f—Ó Suchmaschinen mit hoher Last an.');
+define('_MI_PROTECTOR_DOS_EXPIRE','Zeitlimit für hohe Serverlast (Sekunden)');
+define('_MI_PROTECTOR_DOS_EXPIREDSC','Dieser Wert gibt das Zeitlimit für rasch wiederholte Reloads der Seite (F5 Attacke) und fürSuchmaschinen mit hoher Last an.');
 
-define('_MI_PROTECTOR_DOS_F5COUNT','Anzahl als schçÅlich eingestufter Reloads');
+define('_MI_PROTECTOR_DOS_F5COUNT','Anzahl als schädlich eingestufter Reloads');
 define('_MI_PROTECTOR_DOS_F5COUNTDSC','verhindert DoS Attacken.<br />Der Wert gibt an, wieviele Reloads (F5) als Attacke eingestuft werden.');
-define('_MI_PROTECTOR_DOS_F5ACTION','Massnahmen gegen F5 Attacke');
+define('_MI_PROTECTOR_DOS_F5ACTION','Maßnahmen gegen F5 Attacke');
 
-define('_MI_PROTECTOR_DOS_CRCOUNT','Anzahl als schçÅlich eingestufter Suchmaschinen-Abfragen');
-define('_MI_PROTECTOR_DOS_CRCOUNTDSC','Sch—Õzt vor Server-intensiven Abfragen durch Suchmaschinen.<br />Dieser Wert gibt an, wieviele Zugriffe als Server-intensiv eingestuft werden.');
-define('_MI_PROTECTOR_DOS_CRACTION','Massnahmen gegen Server-intensive Suchmaschinen');
+define('_MI_PROTECTOR_DOS_CRCOUNT','Anzahl als schädlich eingestufter Suchmaschinen-Abfragen');
+define('_MI_PROTECTOR_DOS_CRCOUNTDSC','Schützt vor Server-intensiven Abfragen durch Suchmaschinen.<br />Dieser Wert gibt an, wieviele Zugriffe als Server-intensiv eingestuft werden.');
+define('_MI_PROTECTOR_DOS_CRACTION','Maßnahmen gegen Server-intensive Suchmaschinen');
 
 define('_MI_PROTECTOR_DOS_CRSAFE','Zugelassene User-Agents');
-define('_MI_PROTECTOR_DOS_CRSAFEDSC','Ein regulçÓer Perl-Ausdruck f—Ó User-Agents.<br />Wenn der Ausdruck zutrifft, wird die Suchmaschine niemals als Server-intensiv eingestuft.<br />Bsp: (msnbot|Googlebot|Yahoo! Slurp)/i');
+define('_MI_PROTECTOR_DOS_CRSAFEDSC','Ein regulçÓer Perl-Ausdruck fürUser-Agents.<br />Wenn der Ausdruck zutrifft, wird die Suchmaschine niemals als Server-intensiv eingestuft.<br />Bsp: (msnbot|Googlebot|Yahoo! Slurp)/i');
 
 define('_MI_PROTECTOR_OPT_NONE','Keine (nur logging)');
-define('_MI_PROTECTOR_OPT_SAN','Sanitizing (SçÖberung)');
-define('_MI_PROTECTOR_OPT_EXIT','Weisser Bildschirm');
+define('_MI_PROTECTOR_OPT_SAN','Sanitizing (Säuberung)');
+define('_MI_PROTECTOR_OPT_EXIT','Weißer Bildschirm');
 define('_MI_PROTECTOR_OPT_BIP','IP sperren');
 
 define('_MI_PROTECTOR_DOSOPT_NONE','Keine (nur logging)');
 define('_MI_PROTECTOR_DOSOPT_SLEEP','Sleep');
-define('_MI_PROTECTOR_DOSOPT_EXIT','Weisser Bildschirm');
+define('_MI_PROTECTOR_DOSOPT_EXIT','Weißer Bildschirm');
 define('_MI_PROTECTOR_DOSOPT_BIP','IP sperren');
 
 define('_MI_PROTECTOR_BIP_EXCEPT','Gruppen, die niemals als "schlechte IP" eingestuft werden.');
 define('_MI_PROTECTOR_BIP_EXCEPTDSC','Ein User, der in dieser Gruppe ist, wird niemals eine IP-Sperre erfahren.<br />(Empfohlen wird, die Administartor-Gruppe anzugeben.)');
-define('_MI_PROTECTOR_PATCH2092','Spezielle Massnahmen f—Ó XOOPS <= 2.0.9.2');
+define('_MI_PROTECTOR_PATCH2092','Spezielle Maßnahmen fürXOOPS <= 2.0.9.2');
 define('_MI_PROTECTOR_PASSWD_BIP','Notfall-Passwort (hebt IP-Sperre auf)');
 define('_MI_PROTECTOR_PASSWD_BIPDSC','Wenn Sie Ihre IP gesperrt wird, rufen Sie XOOPS_URL/modules/protector/admin/rescue.php und geben dort das Notfall-Passwort ein.<br />Vergessen Sie nicht, das Passwort zu setzen, bevor sie aufgrund eines Fehlers gesperrt werden.<br />Ist diese Option leer, wird die IP-Sperre nicht funktionieren.');
 

01c24c856e61d86a8559a3efadbeda40914ac8e3
gijoe <gijoe@e2f808cc-50f8-0310-90e1-e20752c72bdc>
2006-07-13 08:30:34 +0000

    tellafriend-1.03

--- trunk/html/modules/tellafriend/xoops_version.php
+++ trunk/html/modules/tellafriend/xoops_version.php
@@ -26,7 +26,7 @@
 //  ------------------------------------------------------------------------ //
 
 $modversion['name'] = _MI_TELLAFRIEND_MODNAME ;
-$modversion['version'] = 1.02;
+$modversion['version'] = 1.03;
 $modversion['description'] = _MI_TELLAFRIEND_MODDESC ;
 $modversion['credits'] = "PEAK Corp. http://www.peak.ne.jp/";
 $modversion['author'] = "GIJOE";
@@ -48,7 +48,7 @@ $modversion['adminindex'] = "admin/index.php";
 $modversion['adminmenu'] = "admin/menu.php";
 
 // Menu
-$modversion['hasMain'] = 0;
+$modversion['hasMain'] = 1;
 
 // Config Settings
 $modversion['hasconfig'] = 1;

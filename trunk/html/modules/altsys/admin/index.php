97249a36b6d9cf4c659f77b9c1bf313a6f243e92
gijoe <gijoe@e2f808cc-50f8-0310-90e1-e20752c72bdc>
2006-10-17 20:42:36 +0000

    altsys-0.20alpha

--- trunk/html/modules/altsys/admin/index.php
+++ trunk/html/modules/altsys/admin/index.php
@@ -8,9 +8,14 @@ $mydirname = basename( dirname( dirname( __FILE__ ) ) ) ;
 $mydirpath = dirname( dirname( __FILE__ ) ) ;
 
 
+if( empty( $xoopsModule ) ) {
+	$moduleperm_handler =& xoops_gethandler( 'module' ) ;
+	$xoopsModule =& $moduleperm_handler->getByDirname( 'altsys' ) ;
+}
+
 // check permission of 'module_admin' of this module
-$moduleperm_handler =& xoops_gethandler( 'groupperm' ) ;
-if( ! is_object( @$xoopsUser ) || ! $moduleperm_handler->checkRight( 'module_admin' , $xoopsModule->getVar( 'mid' ) , $xoopsUser->getGroups() ) ) die( 'only admin can access this area' ) ;
+/* $moduleperm_handler =& xoops_gethandler( 'groupperm' ) ;
+if( ! is_object( @$xoopsUser ) || ! $moduleperm_handler->checkRight( 'module_admin' , $xoopsModule->getVar( 'mid' ) , $xoopsUser->getGroups() ) ) die( 'only admin can access this area' ) ; */
 require XOOPS_ROOT_PATH.'/include/cp_functions.php' ;
 
 $page = preg_replace( '[^a-zA-Z0-9_-]' , '' , @$_GET['page'] ) ;
@@ -23,13 +28,13 @@ if( empty( $_GET['dirname'] ) ) {
 	$_GET['dirname'] = $top_module->getVar('dirname') ;
 }
 
-
 // language file
 if( file_exists( dirname(__FILE__).'/language/'.$xoopsConfig['language'].'/'.$page.'.php' ) ) {
 	include_once dirname(__FILE__).'/language/'.$xoopsConfig['language'].'/'.$page.'.php' ;
 } else if( file_exists( dirname(__FILE__).'/language/english/'.$page.'.php' ) ) {
 	include_once dirname(__FILE__).'/language/english/'.$page.'.php' ;
 }
+
 
 if( file_exists( XOOPS_TRUST_PATH.'/libs/altsys/'.$page.'.php' ) ) {
 	include XOOPS_TRUST_PATH.'/libs/altsys/'.$page.'.php' ;

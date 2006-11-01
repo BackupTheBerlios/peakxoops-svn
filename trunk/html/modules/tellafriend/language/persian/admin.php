5c2bf1071ad1e6728236735e0013f8e5d8a61382
gijoe <gijoe@e2f808cc-50f8-0310-90e1-e20752c72bdc>
2006-11-01 20:46:29 +0000

    tellafriend-1.03a

---
+++ trunk/html/modules/tellafriend/language/persian/admin.php
@@ -0,0 +1,16 @@
+<?php
+
+// index.php
+define("_AM_TH_DATETIME","زمان");
+define("_AM_TH_USER","کاربر");
+define("_AM_TH_IP","IP");
+define("_AM_TH_AGENT","نماینده");
+define("_AM_TH_MAIL_FROM","از:");
+define("_AM_TH_MAIL_TO","به:");
+
+define( "_AM_LABEL_REMOVE" , "پاک کردن اسناد  چک شده:" ) ;
+define( "_AM_BUTTON_REMOVE" , "پاک کردن" ) ;
+define( "_AM_JS_REMOVECONFIRM" , "آیا پاک شود" ) ;
+define( "_AM_MSG_REMOVED" , "اسناد پاک شد" ) ;
+
+?>

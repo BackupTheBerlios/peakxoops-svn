7da993c7a9f15f3f71627361b3028219abbc2f92
gijoe <gijoe@e2f808cc-50f8-0310-90e1-e20752c72bdc>
2006-12-23 21:56:48 +0000

    pico-0.22a

--- trunk/xoops_trust_path/modules/pico/blocks/list.php
+++ trunk/xoops_trust_path/modules/pico/blocks/list.php
@@ -56,7 +56,7 @@ function b_pico_list_show( $options )
 		$categories4assign = implode(',',$categories) ;
 	}
 
-	$sql = "SELECT o.content_id,o.subject,o.created_time,o.modified_time,o.poster_uid,c.cat_title FROM ".$db->prefix($mydirname."_contents")." o LEFT JOIN ".$db->prefix($mydirname."_categories")." c ON o.cat_id=c.cat_id WHERE ($whr_read4cat) AND ($whr_categories) AND o.visible ORDER BY $selected_order,o.content_id LIMIT $contents_num" ;
+	$sql = "SELECT o.content_id,o.subject,o.created_time,o.modified_time,o.poster_uid,c.cat_id,c.cat_title FROM ".$db->prefix($mydirname."_contents")." o LEFT JOIN ".$db->prefix($mydirname."_categories")." c ON o.cat_id=c.cat_id WHERE ($whr_read4cat) AND ($whr_categories) AND o.visible ORDER BY $selected_order,o.content_id LIMIT $contents_num" ;
 	if( ! $result = $db->query( $sql ) ) {
 		echo $db->logger->dumpQueries() ;
 		exit ;
@@ -70,6 +70,8 @@ function b_pico_list_show( $options )
 		'mod_imageurl' => XOOPS_URL.'/modules/'.$mydirname.'/'.$configs['images_dir'] ,
 		'mod_config' => $configs ,
 		'categories' => $categories4assign ,
+		'lang_category' => constant($constpref.'_CATEGORY') ,
+		'lang_topcategory' => constant($constpref.'_TOPCATEGORY') ,
 	) ;
 
 	while( $content_row = $db->fetchArray( $result ) ) {
@@ -81,6 +83,7 @@ function b_pico_list_show( $options )
 			'modified_time' => $content_row['modified_time'] ,
 			'modified_time_formatted' => formatTimestamp( $content_row['modified_time'] ) ,
 			'poster_uid' => $content_row['poster_uid'] ,
+			'cat_id' => intval( $content_row['cat_id'] ) ,
 			'cat_title' => $myts->makeTboxData4Show( $content_row['cat_title'] ) ,
 		) ;
 	}

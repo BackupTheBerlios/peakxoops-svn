cf7054e43abd8590ec37b5dab5e36ef34cde8866
gijoe <gijoe@e2f808cc-50f8-0310-90e1-e20752c72bdc>
2007-01-01 22:32:03 +0000

    pico-0.32a

--- trunk/xoops_trust_path/modules/pico/include/menu.inc.php
+++ trunk/xoops_trust_path/modules/pico/include/menu.inc.php
@@ -1,38 +1,47 @@
 <?php
 
-// contents loop
-$sql = "SELECT o.*,c.cat_id,c.cat_title,c.cat_depth_in_tree FROM ".$db->prefix($mydirname."_contents")." o LEFT JOIN ".$db->prefix($mydirname."_categories")." c ON o.cat_id=c.cat_id WHERE ($whr_read4content) AND o.visible AND o.show_in_menu ORDER BY c.cat_order_in_tree,o.weight" ;
-if( ! $result = $db->query( $sql ) ) {
+// top category
+$categories4assign = array() ;
+if( ! empty( $category_permissions[0] ) ) {
+	$categories4assign[0] = array(
+		'id' => 0 ,
+		'title' => $xoopsModule->getVar('name') ,
+		'depth_in_tree' => 0 ,
+	) ;
+}
+
+// categories loop
+$sql = "SELECT c.* FROM ".$db->prefix($mydirname."_categories")." c WHERE ($whr_read4cat) ORDER BY c.cat_order_in_tree" ;
+if( ! $crs = $db->query( $sql ) ) {
 	echo $db->logger->dumpQueries() ;
 	exit ;
 }
-
-while( $content_row = $db->fetchArray( $result ) ) {
-	$cat_id = intval( $content_row['cat_id'] ) ;
-	if( empty( $cat4assign[$cat_id] ) ) {
-		if( $cat_id == 0 ) {
-			$cat4assign[$cat_id] = array(
-				'id' => 0 ,
-				'title' => $xoopsModule->getVar('name') ,
-				'depth_in_tree' => 0 ,
-			) ;
-		} else {
-			$cat4assign[$cat_id] = array(
-				'id' => intval( $content_row['cat_id'] ) ,
-				'title' => $myts->makeTboxData4Show( $content_row['cat_title'] ) ,
-				'depth_in_tree' => $content_row['cat_depth_in_tree'] + 1 ,
-			) ;
-		}
-	}
-	$content4assign = array(
-		'id' => intval( $content_row['content_id'] ) ,
-		'subject' => $myts->makeTboxData4Show( $content_row['subject'] ) ,
-		'created_time_formatted' => formatTimestamp( $content_row['created_time'] ) ,
-		'modified_time_formatted' => formatTimestamp( $content_row['modified_time'] ) ,
+while( $cat_row = $db->fetchArray( $crs ) ) {
+	$cat_id = intval( $cat_row['cat_id'] ) ;
+	$category4assign = array(
+		'id' => intval( $cat_row['cat_id'] ) ,
+		'title' => $myts->makeTboxData4Show( $cat_row['cat_title'] ) ,
+		'depth_in_tree' => $cat_row['cat_depth_in_tree'] + 1 ,
 	) ;
-	$cat4assign[$cat_id]['contents'][] = $content4assign + $content_row ;
+	$categories4assign[ $cat_id ] = $category4assign + $cat_row ;
 }
 
-$categories4assign = @$cat4assign ;
+foreach( array_keys( $categories4assign ) as $cat_id ) {
+	// contents loop
+	$sql = "SELECT o.* FROM ".$db->prefix($mydirname."_contents")." o WHERE o.cat_id=$cat_id AND o.visible AND o.show_in_menu ORDER BY o.weight" ;
+	if( ! $ors = $db->query( $sql ) ) {
+		echo $db->logger->dumpQueries() ;
+		exit ;
+	}
+	while( $content_row = $db->fetchArray( $ors ) ) {
+		$content4assign = array(
+			'id' => intval( $content_row['content_id'] ) ,
+			'subject' => $myts->makeTboxData4Show( $content_row['subject'] ) ,
+			'created_time_formatted' => formatTimestamp( $content_row['created_time'] ) ,
+			'modified_time_formatted' => formatTimestamp( $content_row['modified_time'] ) ,
+		) ;
+		$categories4assign[ $cat_id ]['contents'][] = $content4assign + $content_row ;
+	}
+}
 
 ?>

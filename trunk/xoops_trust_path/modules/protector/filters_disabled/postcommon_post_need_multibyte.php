63488219424699cd241ea007acd167d85a8dc74e
gijoe <gijoe@e2f808cc-50f8-0310-90e1-e20752c72bdc>
2007-02-02 18:32:05 +0000

    Protector-3.00beta2a

--- trunk/xoops_trust_path/modules/protector/filters_disabled/postcommon_post_need_multibyte.php
+++ trunk/xoops_trust_path/modules/protector/filters_disabled/postcommon_post_need_multibyte.php
@@ -5,9 +5,13 @@
 
 function protector_postcommon_post_need_multibyte()
 {
+	global $xoopsUser ;
+
 	if( ! function_exists( 'mb_strlen' ) ) return true ;
 
-	global $xoopsUser ;
+	// registered users always pass this plugin
+	if( is_object( $xoopsUser ) ) return true ;
+
 	$protector =& Protector::getInstance() ;
 
 	// dare to ignore arrays
@@ -16,8 +20,7 @@ function protector_postcommon_post_need_multibyte()
 		if( strlen( $data ) > 100 ) {
 			if( strlen( $data ) == mb_strlen( $data ) ) {
 				$protector->message .= "No multibyte character was found ($data)\n" ;
-				$uid = is_object( $xoopsUser ) ? $xoopsUser->getVar('uid') : 0 ;
-				$protector->output_log( 'Singlebyte SPAM' , $uid , false , 128 ) ;
+				$protector->output_log( 'Singlebyte SPAM' , 0 , false , 128 ) ;
 				die( 'Protector rejects your post, because your post looks like SPAM' ) ;
 			}
 		}

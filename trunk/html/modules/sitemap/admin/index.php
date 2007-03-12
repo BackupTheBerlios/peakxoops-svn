17b42f8ba995c119ef4a5b019a6dbce17ffa0f80
gijoe <gijoe@e2f808cc-50f8-0310-90e1-e20752c72bdc>
2007-03-12 03:41:57 +0000

    sitemap-1.30

--- trunk/html/modules/sitemap/admin/index.php
+++ trunk/html/modules/sitemap/admin/index.php
@@ -8,8 +8,8 @@ if( defined( 'XOOPS_TRUST_PATH' ) && ! empty( $_GET['lib'] ) ) {
 	$mydirpath = dirname( dirname( __FILE__ ) ) ;
 
 	// common libs (eg. altsys)
-	$lib = preg_replace( '[^a-zA-Z0-9_-]' , '' , $_GET['lib'] ) ;
-	$page = preg_replace( '[^a-zA-Z0-9_-]' , '' , @$_GET['page'] ) ;
+	$lib = preg_replace( '/[^a-zA-Z0-9_-]/' , '' , $_GET['lib'] ) ;
+	$page = preg_replace( '/[^a-zA-Z0-9_-]/' , '' , @$_GET['page'] ) ;
 	
 	if( file_exists( XOOPS_TRUST_PATH.'/libs/'.$lib.'/'.$page.'.php' ) ) {
 		include XOOPS_TRUST_PATH.'/libs/'.$lib.'/'.$page.'.php' ;

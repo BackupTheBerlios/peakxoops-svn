bb2f7c1158efc7f556c22bf37bfe3b2f156d8b38
gijoe <gijoe@e2f808cc-50f8-0310-90e1-e20752c72bdc>
2007-03-18 20:55:51 +0000

    pico-1.15b

--- trunk/xoops_trust_path/modules/pico/include/rss.inc.php
+++ trunk/xoops_trust_path/modules/pico/include/rss.inc.php
@@ -16,9 +16,9 @@ while( $content_row = $db->fetchArray( $result ) ) {
 		'subject' => $myts->makeTboxData4Show( $content_row['subject'] ) ,
 		'body4rss' => htmlspecialchars( xoops_substr( strip_tags( pico_filter_body( $mydirname , $content_row , $content_row['use_cache'] ) ) , 0 , 255 ) , ENT_QUOTES ) ,
 		'created_time_formatted' => formatTimestamp( $content_row['created_time'] ) ,
-		'created_time4rss' => formatTimestamp( $content_row['created_time'] , 'rss' ) ,
+		'created_time4rss' => date( 'r' , $content_row['created_time'] ) ,
 		'modified_time_formatted' => formatTimestamp( $content_row['modified_time'] ) ,
-		'modified_time4rss' => formatTimestamp( $content_row['modified_time'] , 'rss' ) ,
+		'modified_time4rss' => date( 'r' , $content_row['modified_time'] ) ,
 		'cat_id' => intval( $content_row['cat_id'] ) ,
 		'cat_title' => $myts->makeTboxData4Show( $content_row['cat_title'] ) ,
 	) ;

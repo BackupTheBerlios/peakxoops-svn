8b22c3bfe8668d5dfacd64a5a15601265cfb8954
gijoe <gijoe@e2f808cc-50f8-0310-90e1-e20752c72bdc>
2007-04-26 19:26:04 +0000

    d3pipes-0.13

--- trunk/xoops_trust_path/modules/d3pipes/joints/D3pipesReassignAbstract.class.php
+++ trunk/xoops_trust_path/modules/d3pipes/joints/D3pipesReassignAbstract.class.php
@@ -4,14 +4,14 @@ require_once dirname(__FILE__).'/D3pipesJointAbstract.class.php' ;
 
 class D3pipesReassignAbstract extends D3pipesJointAbstract {
 
-	var $pattern ;
+	var $option ;
 
 	// constructor
 	function D3pipesReassignAbstract( $mydirname , $pipe_id , $option )
 	{
 		$this->mydirname = $mydirname ;
 		$this->pipe_id = intval( $pipe_id ) ;
-		$this->pattern = $option ;
+		$this->option = $option ;
 	}
 	
 	function execute( $entries , $max_entries = 10 ) {}
---
+++ trunk/xoops_trust_path/modules/d3pipes/joints/parse/D3pipesParseSimplehtml.class.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once dirname(dirname(__FILE__)).'/D3pipesParseAbstract.class.php' ;
+
+class D3pipesParseSimplehtml extends D3pipesParseAbstract {
+
+	function execute( $html_source , $max_entries = '' )
+	{
+		$items = array() ;
+
+		preg_match_all( $this->option , $html_source , $matches , PREG_SET_ORDER ) ;
+		foreach( $matches as $match ) {
+			$items[] = array(
+				'headline' => $match[1] ,
+				'pubtime' => time() ,
+				// 'link' => '' ,
+				'fingerprint' => md5( $match[1] ) ,
+			) ;
+		}
+
+		return $items ;
+	}
+
+}
+
+?>

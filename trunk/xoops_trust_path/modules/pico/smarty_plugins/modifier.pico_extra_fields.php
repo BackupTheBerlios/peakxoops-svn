591e4c6291c7dcc687c50fe194a0c0c48175f7d8
gijoe <gijoe@e2f808cc-50f8-0310-90e1-e20752c72bdc>
2007-12-19 03:28:30 +0000

    pico-1.70pre

--- trunk/xoops_trust_path/modules/pico/smarty_plugins/modifier.pico_extra_fields.php
+++ trunk/xoops_trust_path/modules/pico/smarty_plugins/modifier.pico_extra_fields.php
@@ -1,8 +1,25 @@
 <?php
 
-function smarty_modifier_pico_extra_fields( $extra_fields_serialized , $key = '' )
+/**
+ * Smarty plugin
+ */
+
+
+/**
+ * Smarty pico_extra_fields modifier plugin
+ *
+ * Type:     modifier
+ * Name:     extra_fields
+ * Usage:    {"(field_name)"|pico_extra_fields:$content}
+ * @link 
+ * @author   
+ * @param string
+ * @return string
+ */
+
+function smarty_modifier_pico_extra_fields( $key = '' , $content_row )
 {
-	$extra_fields = @unserialize( $extra_fields_serialized ) ;
+	$extra_fields = @unserialize( @$content_row['extra_fields'] ) ;
 	return empty( $key ) ? $extra_fields : @$extra_fields[ $key ] ;
 }
 

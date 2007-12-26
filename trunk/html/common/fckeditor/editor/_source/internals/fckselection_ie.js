b39c96fadbcee490efdafeb9a106f50ba5951c5c
gijoe <gijoe@e2f808cc-50f8-0310-90e1-e20752c72bdc>
2007-12-26 04:14:48 +0000

    fckxoops-2.5.1-000pre

--- trunk/html/common/fckeditor/editor/_source/internals/fckselection_ie.js
+++ trunk/html/common/fckeditor/editor/_source/internals/fckselection_ie.js
@@ -1,27 +1,47 @@
 ﻿/*
- * FCKeditor - The text editor for internet
- * Copyright (C) 2003-2006 Frederico Caldeira Knabben
- * 
- * Licensed under the terms of the GNU Lesser General Public License:
- * 		http://www.opensource.org/licenses/lgpl-license.php
- * 
- * For further information visit:
- * 		http://www.fckeditor.net/
- * 
- * "Support Open Source software. What about a donation today?"
- * 
- * File Name: fckselection_ie.js
- * 	Active selection functions. (IE specific implementation)
- * 
- * File Authors:
- * 		Frederico Caldeira Knabben (fredck@fckeditor.net)
+ * FCKeditor - The text editor for Internet - http://www.fckeditor.net
+ * Copyright (C) 2003-2007 Frederico Caldeira Knabben
+ *
+ * == BEGIN LICENSE ==
+ *
+ * Licensed under the terms of any of the following licenses at your
+ * choice:
+ *
+ *  - GNU General Public License Version 2 or later (the "GPL")
+ *    http://www.gnu.org/licenses/gpl.html
+ *
+ *  - GNU Lesser General Public License Version 2.1 or later (the "LGPL")
+ *    http://www.gnu.org/licenses/lgpl.html
+ *
+ *  - Mozilla Public License Version 1.1 or later (the "MPL")
+ *    http://www.mozilla.org/MPL/MPL-1.1.html
+ *
+ * == END LICENSE ==
+ *
+ * Active selection functions. (IE specific implementation)
  */
 
 // Get the selection type.
 FCKSelection.GetType = function()
 {
-	return FCK.EditorDocument.selection.type ;
-}
+	// It is possible that we can still get a text range object even when type=='None' is returned by IE.
+	// So we'd better check the object returned by createRange() rather than by looking at the type.
+	try
+	{
+		var ieType = FCK.EditorDocument.selection.type ;
+		if ( ieType == 'Control' || ieType == 'Text' )
+			return ieType ;
+
+		if ( FCK.EditorDocument.selection.createRange().parentElement )
+			return 'Text' ;
+	}
+	catch(e)
+	{
+		// Nothing to do, it will return None properly.
+	}
+
+	return 'None' ;
+} ;
 
 // Retrieves the selected element (if any), just in the case that a single
 // element (object like and image or a table) is selected.
@@ -34,41 +54,70 @@ FCKSelection.GetSelectedElement = function()
 		if ( oRange && oRange.item )
 			return FCK.EditorDocument.selection.createRange().item(0) ;
 	}
-}
+	return null ;
+} ;
 
 FCKSelection.GetParentElement = function()
 {
 	switch ( this.GetType() )
 	{
 		case 'Control' :
-			return FCKSelection.GetSelectedElement().parentElement ;
+			var el = FCKSelection.GetSelectedElement() ;
+			return el ? el.parentElement : null ;
+
 		case 'None' :
-			return ;
+			return null ;
+
 		default :
 			return FCK.EditorDocument.selection.createRange().parentElement() ;
 	}
-}
+} ;
+
+FCKSelection.GetBoundaryParentElement = function( startBoundary )
+{
+	switch ( this.GetType() )
+	{
+		case 'Control' :
+			var el = FCKSelection.GetSelectedElement() ;
+			return el ? el.parentElement : null ;
+
+		case 'None' :
+			return null ;
+
+		default :
+			var doc = FCK.EditorDocument ;
+			
+			var range = doc.selection.createRange() ;
+			range.collapse( startBoundary !== false ) ;
+			
+			var el = range.parentElement() ;
+			
+			// It may happen that range is comming from outside "doc", so we
+			// must check it (#1204).
+			return FCKTools.GetElementDocument( el ) == doc ? el : null ;
+	}
+} ;
 
 FCKSelection.SelectNode = function( node )
 {
 	FCK.Focus() ;
 	FCK.EditorDocument.selection.empty() ;
-
-	try 
+	var oRange ;
+	try
 	{
 		// Try to select the node as a control.
-		var oRange = FCK.EditorDocument.body.createControlRange() ;
+		oRange = FCK.EditorDocument.body.createControlRange() ;
 		oRange.addElement( node ) ;
-	} 
-	catch(e) 
+	}
+	catch(e)
 	{
 		// If failed, select it as a text range.
-		var oRange = FCK.EditorDocument.selection.createRange() ;
+		oRange = FCK.EditorDocument.body.createTextRange() ;
 		oRange.moveToElementText( node ) ;
 	}
 
 	oRange.select() ;
-}
+} ;
 
 FCKSelection.Collapse = function( toStart )
 {
@@ -79,7 +128,7 @@ FCKSelection.Collapse = function( toStart )
 		oRange.collapse( toStart == null || toStart === true ) ;
 		oRange.select() ;
 	}
-}
+} ;
 
 // The "nodeTagName" parameter must be Upper Case.
 FCKSelection.HasAncestorNode = function( nodeTagName )
@@ -103,16 +152,19 @@ FCKSelection.HasAncestorNode = function( nodeTagName )
 	}
 
 	return false ;
-}
+} ;
 
 // The "nodeTagName" parameter must be UPPER CASE.
 FCKSelection.MoveToAncestorNode = function( nodeTagName )
 {
-	var oNode ;
+	var oNode, oRange ;
+
+	if ( ! FCK.EditorDocument )
+		return null ;
 
 	if ( FCK.EditorDocument.selection.type == "Control" )
 	{
-		var oRange = FCK.EditorDocument.selection.createRange() ;
+		oRange = FCK.EditorDocument.selection.createRange() ;
 		for ( i = 0 ; i < oRange.length ; i++ )
 		{
 			if (oRange(i).parentNode)
@@ -124,7 +176,7 @@ FCKSelection.MoveToAncestorNode = function( nodeTagName )
 	}
 	else
 	{
-		var oRange  = FCK.EditorDocument.selection.createRange() ;
+		oRange  = FCK.EditorDocument.selection.createRange() ;
 		oNode = oRange.parentElement() ;
 	}
 
@@ -132,7 +184,7 @@ FCKSelection.MoveToAncestorNode = function( nodeTagName )
 		oNode = oNode.parentNode ;
 
 	return oNode ;
-}
+} ;
 
 FCKSelection.Delete = function()
 {
@@ -146,6 +198,4 @@ FCKSelection.Delete = function()
 	}
 
 	return oSel ;
-}
-
-
+} ;

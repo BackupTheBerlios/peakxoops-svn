499a28281297967274c4273f0ba8d2e0ff4faa21
gijoe <gijoe@e2f808cc-50f8-0310-90e1-e20752c72bdc>
2008-04-30 18:53:19 +0000

    d3pipes-0.62

--- trunk/xoops_trust_path/modules/d3pipes/xoops_version.php
+++ trunk/xoops_trust_path/modules/d3pipes/xoops_version.php
@@ -11,7 +11,7 @@ $constpref = '_MI_' . strtoupper( $mydirname ) ;
 
 $modversion['name'] = $mydirname ;
 $modversion['description'] = constant($constpref.'_DESC') ;
-$modversion['version'] = 0.61 ;
+$modversion['version'] = 0.62 ;
 $modversion['credits'] = "PEAK Corp.";
 $modversion['author'] = "GIJ=CHECKMATE<br />PEAK Corp.(http://www.peak.ne.jp/)" ;
 $modversion['help'] = "" ;

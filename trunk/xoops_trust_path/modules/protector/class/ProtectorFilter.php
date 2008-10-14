8371018f5ebd6cc2afdf2b97518ac27ef1a4dc25
gijoe <gijoe@e2f808cc-50f8-0310-90e1-e20752c72bdc>
2008-10-14 19:39:03 +0000

    Protector-3.20b

--- trunk/xoops_trust_path/modules/protector/class/ProtectorFilter.php
+++ trunk/xoops_trust_path/modules/protector/class/ProtectorFilter.php
@@ -10,6 +10,20 @@ class ProtectorFilterAbstract {
 		$lang = empty( $GLOBALS['xoopsConfig']['language'] ) ? 'english' : $GLOBALS['xoopsConfig']['language'] ;
 		@include_once dirname(dirname(__FILE__)).'/language/'.$lang.'/main.php' ;
 	}
+
+	function isMobile()
+	{
+		if( class_exists( 'Wizin_User' ) ) {
+			// WizMobile (gusagi)
+			$user =& Wizin_User::getSingleton();
+			return $user->bIsMobile ;
+		} else if( defined( 'HYP_K_TAI_RENDER' ) && HYP_K_TAI_RENDER ) {
+			// hyp_common ktai-renderer (nao-pon)
+			return true ;
+		} else {
+			return false ;
+		}
+	}
 }
 
 
